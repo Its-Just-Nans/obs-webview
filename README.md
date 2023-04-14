36b6ee9e941727a0c37fee9c63856a187c0e52e8
n4n5 <its.just.n4n5@gmail.com>
2023-04-14 16:11:48 +0200

    fix: push

--- README.md
+++ README.md
@@ -1,1 +1,36 @@
 # obs-webview-poc
+
+## Usage
+
+```sh
+npm install
+node server.js
+# go to http://localhost:8080 on a browser
+# or
+# go on obs and create a "browser" source linked to http://localhost:8080
+```
+
+Then you can edit the page, when you have finish, just hit `[Enter]` to reload
+
+## Improvements idea
+
+> This project is just a POC, I don't think I will updated it
+
+- create a system to design the page on a new URL like `/studio` (with a button "update")
+- try to have only one server for websocket and webserver
+
+## Infos
+
+This first version was using an fetch on an interval, but it's not clean
+
+```js
+setInterval(() => {
+    fetch("http://localhost:8100/needRefresh").then(async (resp) => {
+        const txt = await resp.text();
+        if (txt === "yes") {
+            location.reload();
+        }
+    }).catch((e) => {
+    })
+}, 1000)
+```
---
+++ server.js
@@ -0,0 +1,65 @@
+const http = require("http");
+const fs = require("fs");
+const WebSocket = require("ws");
+
+let needReload = false;
+
+const webPORT = 8080;
+
+const wsServer = new WebSocket.Server({
+    port: 7071,
+});
+
+wsServer.on("connection", function (ws) {
+    console.log("webpage connected");
+});
+
+http.createServer(function (request, response) {
+    response.writeHead(200, {
+        "Content-Type": "text/html; charset=utf-8",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "GET, PUT, POST",
+        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+    });
+    if (request.url === "/") {
+        const streamer = fs.createReadStream("index.html");
+        streamer.pipe(response);
+        streamer.on("end", () => {
+            response.end();
+        });
+    } else if (request.url === "/needRefresh") {
+        response.end(needReload ? "yes" : "no");
+    } else if ("/favicon.ico") {
+        // console.log(request.url)
+    } else {
+        console.log(request.url);
+    }
+}).listen(webPORT);
+
+console.log(`listenning on http://localhost:${webPORT}`);
+
+const readline = require("readline").createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+const question = async () => {
+    return new Promise((res, rej) => {
+        readline.question(`[Enter] to reload\n`, (name) => {
+            wsServer.clients.forEach(function each(client) {
+                if (client.readyState === WebSocket.OPEN) {
+                    // check if client is ready
+                    client.send("reload");
+                }
+            });
+            res();
+        });
+    });
+};
+
+const reload = async () => {
+    question().then(() => {
+        reload();
+    });
+};
+reload();
