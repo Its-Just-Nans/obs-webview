de304eab1ab2aa993a53de8f59c19b1deac780d3
n4n5 <its.just.n4n5@gmail.com>
2023-04-14 20:16:12 +0200

    fix: push

--- server.js
+++ server.js
@@ -6,35 +6,37 @@ let needReload = false;
 
 const webPORT = 8080;
 
+const server = http
+    .createServer(function (request, response) {
+        response.writeHead(200, {
+            "Content-Type": "text/html; charset=utf-8",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, PUT, POST",
+            "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
+        });
+        if (request.url === "/") {
+            const streamer = fs.createReadStream("index.html");
+            streamer.pipe(response);
+            streamer.on("end", () => {
+                response.end();
+            });
+        } else if (request.url === "/needRefresh") {
+            response.end(needReload ? "yes" : "no");
+        } else if ("/favicon.ico") {
+            // console.log(request.url)
+        } else {
+            console.log(request.url);
+        }
+    })
+    .listen(webPORT);
+
 const wsServer = new WebSocket.Server({
-    port: 7071,
+    server,
 });
 
 wsServer.on("connection", function (ws) {
     console.log("webpage connected");
 });
-
-http.createServer(function (request, response) {
-    response.writeHead(200, {
-        "Content-Type": "text/html; charset=utf-8",
-        "Access-Control-Allow-Origin": "*",
-        "Access-Control-Allow-Methods": "GET, PUT, POST",
-        "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept",
-    });
-    if (request.url === "/") {
-        const streamer = fs.createReadStream("index.html");
-        streamer.pipe(response);
-        streamer.on("end", () => {
-            response.end();
-        });
-    } else if (request.url === "/needRefresh") {
-        response.end(needReload ? "yes" : "no");
-    } else if ("/favicon.ico") {
-        // console.log(request.url)
-    } else {
-        console.log(request.url);
-    }
-}).listen(webPORT);
 
 console.log(`listenning on http://localhost:${webPORT}`);
 
